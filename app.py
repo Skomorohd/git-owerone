30fde742269605565ae6de89b74714acf59ba85a
Skomorohd <dmitriy.skomoroh@gmail.com>
2022-02-28 21:45:11 +0300

    edit add gitignore

---
+++ app.py
@@ -0,0 +1,35 @@
+# class SomeClass:
+#     class_arr = "Я аттрибут класса  SomeClass"
+#     def __init__(self, a):
+#         self.a = a
+#
+#     def instanse_func(self):
+#         pass
+#
+#     @classmethod
+#     def class_func(self):
+#         pass
+#
+#     @staticmethod
+#     def static_func(self):
+#         pass
+#
+#
+# print(SomeClass.class_arr)
+#
+# obj1 = SomeClass("100")
+# obj2 = SomeClass("100")
+# print(obj1.class_arr)
+# print(obj1.a)
+#
+#     # def some_func(self):
+#     #     pass
+#
+# # obj1 = SomeClass("Я аттрибут экземпляра obj1")
+# # obj2 = SomeClass("Я аттрибут экземпляра obj2")
+# # print(obj1.a)
+# # print(obj2.a)
+#
+# # print(dir(SomeClass))
+
+
